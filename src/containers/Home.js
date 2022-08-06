0eb056becb3b197e6e4c42ada56c6f18cc74f8b9
Fatou COULIBALY SEILLER <fatoucoulibalyseiller@MacBook-Air-de-Fatou.local>
2022-08-06 17:31:21 +0200

    last

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,9 +5,10 @@ import { useState, useEffect } from "react";
 import banner from "../img/banner.jpg";
 import "../asset/home.scss";
 
-const Home = ({ data, setSwitchPage, switchPage, limit, setLimit }) => {
+const Home = ({ data }) => {
   const [offers, setOffers] = useState({});
   const [isloading, setIsLoading] = useState(true);
+  // const [data, setData] = useState();
 
   const fetchData = async () => {
     const response = await axios.get(
@@ -51,36 +52,45 @@ const Home = ({ data, setSwitchPage, switchPage, limit, setLimit }) => {
             </Link>
           </div>
         </div>
-
-        <main className="offers2">
-          {offers.map((offer, index) => {
+        <div className="center">
+          {offers.map((item, index) => {
             return (
-              <Link
-                to={`/offer/${offer._id}`}
-                style={{ textDecoration: "none" }}
-              >
-                <div key={index}>
-                  <div className="annonce">
-                    <p>{offer.owner.account.username}</p>
-                    {offer.product_pictures[0] ? (
-                      <img
-                        src={offer.product_pictures[0].secure_url}
-                        alt="imgprod"
-                      />
-                    ) : null}
-                    <div className="description">
-                      <h4>{offer.product_price}€</h4>
-
-                      <span>{offer.product_details[0].MARQUE}</span>
-
-                      <span>{offer.product_details[0].TAILLE}</span>
+              <div key={index} className="item">
+                <div className="owner">
+                  {item.owner.account.avatar && (
+                    <img
+                      className="avatar"
+                      src={item.owner.account.avatar.secure_url}
+                      alt="Owner avatar"
+                    />
+                  )}
+                  <div className="username">{item.owner.account.username}</div>
+                </div>
+                <Link className="link" to={`/offer/${item._id}`}>
+                  <div className="item_details">
+                    <img
+                      className="product_image"
+                      src={item.product_image.secure_url}
+                      alt=""
+                    />
+                    <div className="item_details_bottom">
+                      <p className="price">{item.product_price} €</p>
+                      {item.product_details.map((details) => {
+                        if (details.TAILLE) {
+                          return <p> {details.TAILLE} </p>;
+                        }
+                        if (details.MARQUE) {
+                          return <p> {details.MARQUE} </p>;
+                        }
+                        return null;
+                      })}
                     </div>
                   </div>
-                </div>
-              </Link>
+                </Link>
+              </div>
             );
           })}
-        </main>
+        </div>
       </div>
     </div>
   );
